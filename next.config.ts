bf6b268d6c2eda83afc819f6563e05f1953437a6
novinbukannopin <jylovien@gmail.com>
2026-05-24 15:04:16 +0700

    fix: optimizing size and ttl image

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["placehold.co"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+    ],
     qualities: [65, 75],
+    deviceSizes: [640, 828, 1080, 1200, 1920],
+    minimumCacheTTL: 31536000,
   },
 };
 
